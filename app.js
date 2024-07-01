bd90c127cac034f1f89a94d9fae93e95458cb9c3
Fawwaz Maulana <wazwazzsrhrr@gmail.com>
2024-07-01 15:28:39 +0700

    Creating Ionic Elements Programmatically

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-const ReasonInput = document.querySelector('#input-reason');
-const AmountInput = document.querySelector('#input-amount');
-const BtnClose = document.querySelector('#btn-cancel');
-const BtnConfirm = document.querySelector('#btn-confirm');
+const ReasonInput = document.querySelector("#input-reason");
+const AmountInput = document.querySelector("#input-amount");
+const BtnClose = document.querySelector("#btn-cancel");
+const BtnConfirm = document.querySelector("#btn-confirm");
+const expensesList = document.querySelector("#expenses-list");
 
+BtnConfirm.addEventListener("click", () => {
+  const enteredReason = ReasonInput.value;
+  const enteredAmount = AmountInput.value;
 
-
-BtnConfirm.addEventListener('click', () => {
-    const enteredReason = ReasonInput.value;
-    const enteredAmount = AmountInput.value;
-
-    if (enteredReason.trim().length <= 0 || enteredAmount <= 0 || enteredAmount.trim().length <= 0) {
-        return;
-    }
-    console.log(enteredReason, enteredAmount)
+  if (
+    enteredReason.trim().length <= 0 ||
+    enteredAmount <= 0 ||
+    enteredAmount.trim().length <= 0
+  ) {
+    return;
+  }
+  const newItem = document.createElement("ion-item");
+  newItem.textContent = enteredReason + ": $" + enteredAmount;
+  expensesList.appendChild(newItem);
 });
-BtnClose.addEventListener('click', () => {
-    console.log('it works too !!');
-}) 
+BtnClose.addEventListener("click", () => {
+  console.log("it works");
+});
